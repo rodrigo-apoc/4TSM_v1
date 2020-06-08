2f0e48699247ee521f7fffe66616eebd21402997
rodrigo-apoc <rodrigof.lops@gmail.com>
2020-06-08 19:33:42 -0300

    self update test

--- main.sh
+++ main.sh
@@ -38,13 +38,12 @@ self_update() {
     cd $SCRIPTPATH
     git fetch
 
-    [ -n $(git diff --name-only origin/$BRANCH | grep $SCRIPTNAME) ] && {
+    [ -n $(git diff --name-only origin/$BRANCH) ] && {
         echo "Found a new version of me, updating myself..."
         git pull --force
         git checkout $BRANCH
         git pull --force
         echo "Running the new version..."
-        exec "$SCRIPTNAME" "$@"
 
         # Now exit this old instance
         exit 1
