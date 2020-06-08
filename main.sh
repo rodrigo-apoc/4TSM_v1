9c19568fc277bd515675e6f5c1f5f2f36d654419
rodrigo-apoc <rodrigof.lops@gmail.com>
2020-06-08 19:40:34 -0300

    self update test

--- main.sh
+++ main.sh
@@ -28,29 +28,7 @@ echo "##############################################"
 sudo su - csgoserver -c "./csgoserver update"
 
 # 4Teams Server Manager Update #
-SCRIPT=$(readlink -f "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-SCRIPTNAME="$0"
-ARGS="$@"
-BRANCH="master"
-
-self_update() {
-    cd $SCRIPTPATH
-    git fetch
-
-    [ -n $(git diff --name-only origin/$BRANCH) ] && {
-        echo "Found a new version of me, updating myself..."
-        git pull --force
-        git checkout $BRANCH
-        git pull --force
-        echo "Running the new version..."
-        advancedMenu
-
-        # Now exit this old instance
-        exit 1
-    }
-    echo "Already the latest version."
-}
+git pull
 
 # Menu #
 advancedMenu() {
@@ -321,5 +299,4 @@ advancedMenu() {
         clear
 }
 
-self_update
-#advancedMenu
+advancedMenu
